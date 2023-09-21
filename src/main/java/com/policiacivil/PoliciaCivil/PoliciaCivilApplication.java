3104bd22ad892f0ed90f6357506135709da28f87
Gabriel Camargos <86752686+Gabrielcamargos28@users.noreply.github.com>
2023-09-21 10:55:55 -0300

    conexao com postgres no Railway

---
+++ src/main/java/com/policiacivil/PoliciaCivil/PoliciaCivilApplication.java
@@ -0,0 +1,15 @@
+package com.policiacivil.PoliciaCivil;
+
+import jakarta.persistence.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliciaCivilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliciaCivilApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/policiacivil/PoliciaCivil/PoliciaCivilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policiacivil.PoliciaCivil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliciaCivilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
